2d8763e3a12cedc268b3caaca60662b4e518c33a
mayaoy <may61016125@gmail.com>
2014-10-05 23:30:49 +0800

    flie xml

--- savexml_home.php
+++ savexml_home.php
@@ -5,197 +5,205 @@
  	</head>
  	<body>
 		<?php
-			$str = '{"banner":
-[
-{
-"description":"【名山藝術】觀山‧望水-藝術家山水聯展(特別導覽預約)",
-"imageUrl":"http://accupassv3storage.blob.core.windows.net/images/Banner_01.jpg",
-"url":"/event/register/1409241136071389810407",
-"person":"May"},
-{
-"description":"【名山藝術】觀山‧望水-藝術家山水聯展(特別導覽預約)",
-"imageUrl":"http://accupassv3storage.blob.core.windows.net/images/Banner_01.jpg",
-"url":"/event/register/1409241136071389810407",
-"person":"May"},
-{
-"description":"【名山藝術】觀山‧望水-藝術家山水聯展(特別導覽預約)",
-"imageUrl":"http://accupassv3storage.blob.core.windows.net/images/Banner_01.jpg",
-"url":"/event/register/1409241136071389810407",
-"person":"May"},
-{
-"description":"【名山藝術】觀山‧望水-藝術家山水聯展(特別導覽預約)",
-"imageUrl":"http://accupassv3storage.blob.core.windows.net/images/Banner_01.jpg",
-"url":"/event/register/1409241136071389810407",
-"person":"May"},
-{
-"description":"【名山藝術】觀山‧望水-藝術家山水聯展(特別導覽預約)",
-"imageUrl":"http://accupassv3storage.blob.core.windows.net/images/Banner_01.jpg",
-"url":"/event/register/1409241136071389810407",
-"person":"May"}
-],
-"editorChoice":[
-{"key":"game",
-"imageUrl":"http://accupassv3storage.blob.core.windows.net/images/game_home.jpg",
-"zh-tw":"遊戲",
-"en-us":"Games"},
+// 			$str = '{"banner":
+// [
+// {
+// "description":"【名山藝術】觀山‧望水-藝術家山水聯展(特別導覽預約)",
+// "imageUrl":"http://accupassv3storage.blob.core.windows.net/images/Banner_01.jpg",
+// "url":"/event/register/1409241136071389810407",
+// "person":"May"},
+// {
+// "description":"【名山藝術】觀山‧望水-藝術家山水聯展(特別導覽預約)",
+// "imageUrl":"http://accupassv3storage.blob.core.windows.net/images/Banner_01.jpg",
+// "url":"/event/register/1409241136071389810407",
+// "person":"May"},
+// {
+// "description":"【名山藝術】觀山‧望水-藝術家山水聯展(特別導覽預約)",
+// "imageUrl":"http://accupassv3storage.blob.core.windows.net/images/Banner_01.jpg",
+// "url":"/event/register/1409241136071389810407",
+// "person":"May"},
+// {
+// "description":"【名山藝術】觀山‧望水-藝術家山水聯展(特別導覽預約)",
+// "imageUrl":"http://accupassv3storage.blob.core.windows.net/images/Banner_01.jpg",
+// "url":"/event/register/1409241136071389810407",
+// "person":"May"},
+// {
+// "description":"【名山藝術】觀山‧望水-藝術家山水聯展(特別導覽預約)",
+// "imageUrl":"http://accupassv3storage.blob.core.windows.net/images/Banner_01.jpg",
+// "url":"/event/register/1409241136071389810407",
+// "person":"May"}
+// ],
+// "editorChoice":[
+// {"key":"game",
+// "imageUrl":"http://accupassv3storage.blob.core.windows.net/images/game_home.jpg",
+// "zh-tw":"遊戲",
+// "en-us":"Games"},
 
-{"key":"game",
-"imageUrl":"http://accupassv3storage.blob.core.windows.net/images/game_home.jpg",
-"zh-tw":"遊戲",
-"en-us":"Games"},
+// {"key":"game",
+// "imageUrl":"http://accupassv3storage.blob.core.windows.net/images/game_home.jpg",
+// "zh-tw":"遊戲",
+// "en-us":"Games"},
 
-{"key":"game",
-"imageUrl":"http://accupassv3storage.blob.core.windows.net/images/game_home.jpg",
-"zh-tw":"遊戲",
-"en-us":"Games"},
+// {"key":"game",
+// "imageUrl":"http://accupassv3storage.blob.core.windows.net/images/game_home.jpg",
+// "zh-tw":"遊戲",
+// "en-us":"Games"},
 
-{"key":"game",
-"imageUrl":"http://accupassv3storage.blob.core.windows.net/images/game_home.jpg",
-"zh-tw":"遊戲",
-"en-us":"Games"}
-],
+// {"key":"game",
+// "imageUrl":"http://accupassv3storage.blob.core.windows.net/images/game_home.jpg",
+// "zh-tw":"遊戲",
+// "en-us":"Games"}
+// ],
 
-"fresh":[{
-"description":"粉紅10月一起瑜珈 支持奮鬥中的乳癌姊妹",
-"imageUrl":"http://accupassv3storage.blob.core.windows.net/images/fun_01.jpg",
-"url":"http://blog.accupass.com/pink-oct.html"},
-{
-"description":"粉紅10月一起瑜珈 支持奮鬥中的乳癌姊妹",
-"imageUrl":"http://accupassv3storage.blob.core.windows.net/images/fun_01.jpg",
-"url":"http://blog.accupass.com/pink-oct.html"},
-{
-"description":"粉紅10月一起瑜珈 支持奮鬥中的乳癌姊妹",
-"imageUrl":"http://accupassv3storage.blob.core.windows.net/images/fun_01.jpg",
-"url":"http://blog.accupass.com/pink-oct.html"}
-],
-"place":[
-{
-"title":"松江拾壹 Junction",
-"imageUrl":"http://accupassv3storage.blob.core.windows.net/images/space_01.jpg",
-"url":"http://blog.accupass.com/space-junction.html",
-"description":"人文與藝術匯集的音樂殿堂",
-"place":"臺北"
-},
-{
-"title":"松江拾壹 Junction",
-"imageUrl":"http://accupassv3storage.blob.core.windows.net/images/space_01.jpg",
-"url":"http://blog.accupass.com/space-junction.html",
-"description":"人文與藝術匯集的音樂殿堂",
-"place":"臺北"
-},
-{
-"title":"松江拾壹 Junction",
-"imageUrl":"http://accupassv3storage.blob.core.windows.net/images/space_01.jpg",
-"url":"http://blog.accupass.com/space-junction.html",
-"description":"人文與藝術匯集的音樂殿堂",
-"place":"臺北"
-}
-],
-"organizer":[
-{
-"description":"MiniCook迷你酷",
-"imageUrl":"/images/index/logo_mini_cook.png",
-"url":"http://www.accupass.com/org/detail/r/1408110503182085074980/1/0"
-},
-{
-"description":"MiniCook迷你酷",
-"imageUrl":"/images/index/logo_mini_cook.png",
-"url":"http://www.accupass.com/org/detail/r/1408110503182085074980/1/0"
-},
-{
-"description":"MiniCook迷你酷",
-"imageUrl":"/images/index/logo_mini_cook.png",
-"url":"http://www.accupass.com/org/detail/r/1408110503182085074980/1/0"
-},
-{
-"description":"MiniCook迷你酷",
-"imageUrl":"/images/index/logo_mini_cook.png",
-"url":"http://www.accupass.com/org/detail/r/1408110503182085074980/1/0"
-},
-{
-"description":"MiniCook迷你酷",
-"imageUrl":"/images/index/logo_mini_cook.png",
-"url":"http://www.accupass.com/org/detail/r/1408110503182085074980/1/0"
-},
-{
-"description":"MiniCook迷你酷",
-"imageUrl":"/images/index/logo_mini_cook.png",
-"url":"http://www.accupass.com/org/detail/r/1408110503182085074980/1/0"
-},
-{
-"description":"MiniCook迷你酷",
-"imageUrl":"/images/index/logo_mini_cook.png",
-"url":"http://www.accupass.com/org/detail/r/1408110503182085074980/1/0"
-},
-{
-"description":"MiniCook迷你酷",
-"imageUrl":"/images/index/logo_mini_cook.png",
-"url":"http://www.accupass.com/org/detail/r/1408110503182085074980/1/0"
-},
-{
-"description":"MiniCook迷你酷",
-"imageUrl":"/images/index/logo_mini_cook.png",
-"url":"http://www.accupass.com/org/detail/r/1408110503182085074980/1/0"
-},
-{
-"description":"MiniCook迷你酷",
-"imageUrl":"/images/index/logo_mini_cook.png",
-"url":"http://www.accupass.com/org/detail/r/1408110503182085074980/1/0"
-},
-{
-"description":"MiniCook迷你酷",
-"imageUrl":"/images/index/logo_mini_cook.png",
-"url":"http://www.accupass.com/org/detail/r/1408110503182085074980/1/0"
-},
-{
-"description":"MiniCook迷你酷",
-"imageUrl":"/images/index/logo_mini_cook.png",
-"url":"http://www.accupass.com/org/detail/r/1408110503182085074980/1/0"
-},
-{
-"description":"MiniCook迷你酷",
-"imageUrl":"/images/index/logo_mini_cook.png",
-"url":"http://www.accupass.com/org/detail/r/1408110503182085074980/1/0"
-},
-{
-"description":"MiniCook迷你酷",
-"imageUrl":"/images/index/logo_mini_cook.png",
-"url":"http://www.accupass.com/org/detail/r/1408110503182085074980/1/0"
-},
-{
-"description":"MiniCook迷你酷",
-"imageUrl":"/images/index/logo_mini_cook.png",
-"url":"http://www.accupass.com/org/detail/r/1408110503182085074980/1/0"
-},
-{
-"description":"MiniCook迷你酷",
-"imageUrl":"/images/index/logo_mini_cook.png",
-"url":"http://www.accupass.com/org/detail/r/1408110503182085074980/1/0"
-},
-{
-"description":"MiniCook迷你酷",
-"imageUrl":"/images/index/logo_mini_cook.png",
-"url":"http://www.accupass.com/org/detail/r/1408110503182085074980/1/0"
-},
-{
-"description":"MiniCook迷你酷",
-"imageUrl":"/images/index/logo_mini_cook.png",
-"url":"http://www.accupass.com/org/detail/r/1408110503182085074980/1/0"
-}
-]
-}
-';
-$str = str_replace("\n", "", $str);
+// "fresh":[{
+// "description":"粉紅10月一起瑜珈 支持奮鬥中的乳癌姊妹",
+// "imageUrl":"http://accupassv3storage.blob.core.windows.net/images/fun_01.jpg",
+// "url":"http://blog.accupass.com/pink-oct.html"},
+// {
+// "description":"粉紅10月一起瑜珈 支持奮鬥中的乳癌姊妹",
+// "imageUrl":"http://accupassv3storage.blob.core.windows.net/images/fun_01.jpg",
+// "url":"http://blog.accupass.com/pink-oct.html"},
+// {
+// "description":"粉紅10月一起瑜珈 支持奮鬥中的乳癌姊妹",
+// "imageUrl":"http://accupassv3storage.blob.core.windows.net/images/fun_01.jpg",
+// "url":"http://blog.accupass.com/pink-oct.html"}
+// ],
+// "place":[
+// {
+// "title":"松江拾壹 Junction",
+// "imageUrl":"http://accupassv3storage.blob.core.windows.net/images/space_01.jpg",
+// "url":"http://blog.accupass.com/space-junction.html",
+// "description":"人文與藝術匯集的音樂殿堂",
+// "place":"臺北"
+// },
+// {
+// "title":"松江拾壹 Junction",
+// "imageUrl":"http://accupassv3storage.blob.core.windows.net/images/space_01.jpg",
+// "url":"http://blog.accupass.com/space-junction.html",
+// "description":"人文與藝術匯集的音樂殿堂",
+// "place":"臺北"
+// },
+// {
+// "title":"松江拾壹 Junction",
+// "imageUrl":"http://accupassv3storage.blob.core.windows.net/images/space_01.jpg",
+// "url":"http://blog.accupass.com/space-junction.html",
+// "description":"人文與藝術匯集的音樂殿堂",
+// "place":"臺北"
+// }
+// ],
+// "organizer":[
+// {
+// "description":"MiniCook迷你酷",
+// "imageUrl":"/images/index/logo_mini_cook.png",
+// "url":"http://www.accupass.com/org/detail/r/1408110503182085074980/1/0"
+// },
+// {
+// "description":"MiniCook迷你酷",
+// "imageUrl":"/images/index/logo_mini_cook.png",
+// "url":"http://www.accupass.com/org/detail/r/1408110503182085074980/1/0"
+// },
+// {
+// "description":"MiniCook迷你酷",
+// "imageUrl":"/images/index/logo_mini_cook.png",
+// "url":"http://www.accupass.com/org/detail/r/1408110503182085074980/1/0"
+// },
+// {
+// "description":"MiniCook迷你酷",
+// "imageUrl":"/images/index/logo_mini_cook.png",
+// "url":"http://www.accupass.com/org/detail/r/1408110503182085074980/1/0"
+// },
+// {
+// "description":"MiniCook迷你酷",
+// "imageUrl":"/images/index/logo_mini_cook.png",
+// "url":"http://www.accupass.com/org/detail/r/1408110503182085074980/1/0"
+// },
+// {
+// "description":"MiniCook迷你酷",
+// "imageUrl":"/images/index/logo_mini_cook.png",
+// "url":"http://www.accupass.com/org/detail/r/1408110503182085074980/1/0"
+// },
+// {
+// "description":"MiniCook迷你酷",
+// "imageUrl":"/images/index/logo_mini_cook.png",
+// "url":"http://www.accupass.com/org/detail/r/1408110503182085074980/1/0"
+// },
+// {
+// "description":"MiniCook迷你酷",
+// "imageUrl":"/images/index/logo_mini_cook.png",
+// "url":"http://www.accupass.com/org/detail/r/1408110503182085074980/1/0"
+// },
+// {
+// "description":"MiniCook迷你酷",
+// "imageUrl":"/images/index/logo_mini_cook.png",
+// "url":"http://www.accupass.com/org/detail/r/1408110503182085074980/1/0"
+// },
+// {
+// "description":"MiniCook迷你酷",
+// "imageUrl":"/images/index/logo_mini_cook.png",
+// "url":"http://www.accupass.com/org/detail/r/1408110503182085074980/1/0"
+// },
+// {
+// "description":"MiniCook迷你酷",
+// "imageUrl":"/images/index/logo_mini_cook.png",
+// "url":"http://www.accupass.com/org/detail/r/1408110503182085074980/1/0"
+// },
+// {
+// "description":"MiniCook迷你酷",
+// "imageUrl":"/images/index/logo_mini_cook.png",
+// "url":"http://www.accupass.com/org/detail/r/1408110503182085074980/1/0"
+// },
+// {
+// "description":"MiniCook迷你酷",
+// "imageUrl":"/images/index/logo_mini_cook.png",
+// "url":"http://www.accupass.com/org/detail/r/1408110503182085074980/1/0"
+// },
+// {
+// "description":"MiniCook迷你酷",
+// "imageUrl":"/images/index/logo_mini_cook.png",
+// "url":"http://www.accupass.com/org/detail/r/1408110503182085074980/1/0"
+// },
+// {
+// "description":"MiniCook迷你酷",
+// "imageUrl":"/images/index/logo_mini_cook.png",
+// "url":"http://www.accupass.com/org/detail/r/1408110503182085074980/1/0"
+// },
+// {
+// "description":"MiniCook迷你酷",
+// "imageUrl":"/images/index/logo_mini_cook.png",
+// "url":"http://www.accupass.com/org/detail/r/1408110503182085074980/1/0"
+// },
+// {
+// "description":"MiniCook迷你酷",
+// "imageUrl":"/images/index/logo_mini_cook.png",
+// "url":"http://www.accupass.com/org/detail/r/1408110503182085074980/1/0"
+// },
+// {
+// "description":"MiniCook迷你酷",
+// "imageUrl":"/images/index/logo_mini_cook.png",
+// "url":"http://www.accupass.com/org/detail/r/1408110503182085074980/1/0"
+// }
+// ]
+// }
+// ';
+// $str = str_replace("\n", "", $str);
 // echo $str;
 // return;
 
 
 
-			$_POST = json_decode($str, true);
+			// $_POST = json_decode($str, true);
 			//print_r($_POST);
 
+			class SimpleXMLExtended extends SimpleXMLElement {
+			  public function addCData($cdata_text) {
+			    $node = dom_import_simplexml($this); 
+			    $no   = $node->ownerDocument; 
+			    $node->appendChild($no->createCDATASection($cdata_text)); 
+			  } 
+			}
 
-			$xml = new SimpleXMLElement('<?xml version="1.0" encoding="utf-8" ?><home/>');
+
+			$xml = new SimpleXMLExtended('<?xml version="1.0" encoding="utf-8" ?><home/>');
 
 			$organizer_count = 0;
 
@@ -226,24 +234,25 @@ $str = str_replace("\n", "", $str);
 						}
 
 						if ($content_name == 'description' || $content_name == 'title'){
-							$value = '<![CDATA['.$value.']]>';
+							// $value = '<![CDATA['.$value.']]>';
+							$child_item->addCData($content_name,$value);
+						}else{
+							$child_item->addChild($content_name,$value);
 						}
-						$child_item->addChild($content_name,$value);
+						
 					}
 				}
 			}
-			//echo $xml;
-			//print $xml->asXML();
 
 			$dom = new DOMDocument('1.0');
 			$dom->preserveWhiteSpace = false;
 			$dom->formatOutput = true;
 			$dom->loadXML($xml->asXML());
-			echo $dom->saveXML();
-
-
-			//saving generated xml file
-			//$xml_student_info->asXML('file path and name');
+			// echo $dom->saveXML();
+			$xml_str =  $dom->saveXML();
+			$file = fopen(time().'.xml', 'w');
+			fwrite($file, $xml_str);
+			fclose($file);
 
 		?>
  </body>
